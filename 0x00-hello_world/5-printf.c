b871c1e35c900d49bc27d66e74435f9690827fce
Djiela Parfait <djielaparfait99@gmail.com>
2022-09-08 07:45:24 -0700

    task 5_2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Description: prints "with proper grammar, but the outcome is a piece of art,\n"
+ * Description: prints "with proper grammar,but the outcome is a piece of art,"
  * Return: 0
 */
 
