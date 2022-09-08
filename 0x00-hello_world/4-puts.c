381fde6fa159837171111145600f6ccfdd4b4c93
Djiela Parfait <djielaparfait99@gmail.com>
2022-09-08 07:00:43 -0700

    task 4_1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,13 @@
 
 
 /*
- * A C program that prints "Programming is like building a multilingual puzzle"
+ * main - entry point
+ * A C program that prints "Programming is like building a multilingual puzzle
+ * Return: 0
 */
 
 
 int main(void){
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
